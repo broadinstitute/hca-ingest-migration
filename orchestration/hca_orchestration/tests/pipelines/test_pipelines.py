a63feab24f62f051cd8b65e97b2a93e27b25cc67
bahill <bhill@broadinstitute.org>
2025-02-04 14:23:42 -0500

    fixed import for ExecuteInProcessResult

--- orchestration/hca_orchestration/tests/pipelines/test_pipelines.py
+++ orchestration/hca_orchestration/tests/pipelines/test_pipelines.py
@@ -4,8 +4,9 @@ from unittest.mock import MagicMock, Mock, patch
 
 import hca_orchestration.resources.data_repo_service
 import pytest
-from dagster import Failure, JobDefinition, ResourceDefinition, file_relative_path
-from dagster.core.execution import ExecuteInProcessResult
+from dagster import Failure, JobDefinition, ResourceDefinition, file_relative_path, ExecuteInProcessResult
+# TODO looks like this moved
+# from dagster.core.execution import ExecuteInProcessResult
 from dagster.utils import load_yaml_from_globs
 from dagster.utils.merger import deep_merge_dicts
 from dagster_utils.resources.sam import Sam
