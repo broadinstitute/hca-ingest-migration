4756483ed9bfc8d0a25437ae239b3f733676d536
bahill <bhill@broadinstitute.org>
2025-04-17 12:44:41 -0400

    refactoring to use JobDefinition.execute_in_process instead of execute_pipeline which has been removed.
    https://github.com/dagster-io/dagster/blob/master/MIGRATION.md#migrating-to-10

--- orchestration/hca_orchestration/tests/e2e/test_load_null_file_refs.py
+++ orchestration/hca_orchestration/tests/e2e/test_load_null_file_refs.py
@@ -1,5 +1,5 @@
 import pytest
-from dagster import execute_pipeline, Failure
+from dagster import Failure
 from google.cloud.bigquery.client import Client, QueryJobConfig
 
 from hca_orchestration.repositories.local_repository import load_hca_job
@@ -14,10 +14,8 @@ def test_load_null_file_refs(load_hca_run_config, dataset_name, tdr_bigquery_cli
     job = load_hca_job()
 
     with pytest.raises(NullFileIdException):
-        execute_pipeline(
-            job,
-            run_config=load_hca_run_config
-        )
+        result = job.execute_in_process(run_config=load_hca_run_config)
+        assert not result.success
 
     bq_project = dataset_info.dataset_data_project_id
     assert_no_null_file_refs("analysis_file", dataset_name, bq_project, tdr_bigquery_client)
