86516baae4fa36cff44e55e391e23b2b0db86cab
bahill <bhill@broadinstitute.org>
2025-05-15 15:48:38 -0400

    Refactor to remove deprecated APIs - ModeDefinition, ResourceDefinition, execute_solid - and replace with build_op_context

--- orchestration/hca_orchestration/tests/solids/test_data_repo.py
+++ orchestration/hca_orchestration/tests/solids/test_data_repo.py
@@ -1,9 +1,8 @@
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import Mock
 
-from dagster import execute_solid, Failure, ModeDefinition, ResourceDefinition
+from dagster import build_op_context, Failure
 from data_repo_client import RepositoryApi
-
 from hca_manage.common import JobId
 from hca_orchestration.solids.data_repo import base_wait_for_job_completion
 
@@ -24,25 +23,7 @@ data_repo = Mock(spec=RepositoryApi)
 
 
 class WaitForJobCompletionTestCase(unittest.TestCase):
-    def setUp(self):
-        self.test_mode = ModeDefinition(
-            name="test",
-            resource_defs={
-                "data_repo_client": ResourceDefinition.hardcoded_resource(data_repo)
-            }
-        )
-
     def test_polls_repeatedly_until_complete(self):
-        solid_config = {
-            "solids": {
-                "base_wait_for_job_completion": {
-                    "config": {
-                        "poll_interval_seconds": 0,
-                        "max_wait_time_seconds": 10,
-                    }
-                }
-            }
-        }
         job_status_sequence = [
             mock_job_status(False),
             mock_job_status(False),
@@ -50,62 +31,43 @@ class WaitForJobCompletionTestCase(unittest.TestCase):
         ]
 
         data_repo.retrieve_job = Mock(side_effect=job_status_sequence)
-        self.test_mode.resource_defs["data_repo_client"] = ResourceDefinition.hardcoded_resource(data_repo)
 
-        result = execute_solid(
-            base_wait_for_job_completion,
-            run_config=solid_config,
-            mode_def=self.test_mode,
-            input_values={
-                'job_id': JobId('steve-was-here'),
-            })
+        context = build_op_context(
+            resources={"data_repo_client": data_repo},
+            op_config={
+                'poll_interval_seconds': 0,
+                'max_wait_time_seconds': 10,
+            }
+        )
+
+        result = base_wait_for_job_completion(context, job_id=JobId('steve-was-here'))
         self.assertTrue(result.success)
         self.assertEqual(data_repo.retrieve_job.call_count, 3)
 
     def test_fails_if_job_failed(self):
-        solid_config = {
-            "solids": {
-                "base_wait_for_job_completion": {
-                    "config": {
-                        "poll_interval_seconds": 0,
-                        "max_wait_time_seconds": 10,
-                    }
-                }
-            }
-        }
-
         data_repo.retrieve_job = Mock(return_value=mock_job_status(completed=True, successful=False))
-        self.test_mode.resource_defs["data_repo_client"] = ResourceDefinition.hardcoded_resource(data_repo)
+
+        context = build_op_context(
+            resources={"data_repo_client": data_repo},
+            op_config={
+                'poll_interval_seconds': 0,
+                'max_wait_time_seconds': 10,
+            }
+        )
 
         with self.assertRaisesRegex(Failure, f"job_id test_job did not complete successfully."):
-            execute_solid(
-                base_wait_for_job_completion,
-                run_config=solid_config,
-                mode_def=self.test_mode,
-                input_values={
-                    'job_id': JobId('test_job'),
-                })
+            base_wait_for_job_completion(context, job_id=JobId('test_job'))
 
     def test_fails_if_max_time_exceeded(self):
-        solid_config = {
-            "solids": {
-                "base_wait_for_job_completion": {
-                    "config": {
-                        "poll_interval_seconds": 1,
-                        "max_wait_time_seconds": 1,
-                    }
-                }
-            }
-        }
-
         data_repo.retrieve_job = Mock(return_value=mock_job_status(False))
-        self.test_mode.resource_defs["data_repo_client"] = ResourceDefinition.hardcoded_resource(data_repo)
+
+        context = build_op_context(
+            resources={"data_repo_client": data_repo},
+            op_config={
+                'poll_interval_seconds': 0,
+                'max_wait_time_seconds': 1,
+            }
+        )
 
         with self.assertRaisesRegex(Failure, "Exceeded max wait time"):
-            execute_solid(
-                base_wait_for_job_completion,
-                run_config=solid_config,
-                mode_def=self.test_mode,
-                input_values={
-                    'job_id': JobId('steve-was-here'),
-                })
+            base_wait_for_job_completion(context, job_id=JobId('steve-was-here'))
