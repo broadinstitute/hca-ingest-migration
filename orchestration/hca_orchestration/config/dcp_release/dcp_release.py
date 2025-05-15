df68ef040e579d356d8a54ec24ced704b82aefe3
bahill <bhill@broadinstitute.org>
2025-05-15 15:42:43 -0400

    Refactored to no longer use deprecated Partition API - now have partition_keys

--- orchestration/hca_orchestration/config/dcp_release/dcp_release.py
+++ orchestration/hca_orchestration/config/dcp_release/dcp_release.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
 
-from dagster import Partition, file_relative_path
+import dagster as dg
 from dagster.utils import load_yaml_from_path
 from dagster_utils.typing import DagsterObjectConfigSchema
 
@@ -10,14 +10,14 @@ from dagster_utils.typing import DagsterObjectConfigSchema
 from hca_orchestration.support.matchers import find_project_id_in_str
 
 
-def run_config_for_dcp_release_partition(partition: Partition) -> DagsterObjectConfigSchema:
-    path = file_relative_path(
+def run_config_for_dcp_release_partition(partition_key:str) -> DagsterObjectConfigSchema:
+    path = dg.file_relative_path(
         __file__, os.path.join("./run_config/prod", "dcp_release.yaml")
     )
 
     # jscpd:ignore-start
     run_config: DagsterObjectConfigSchema = load_yaml_from_path(path)
-    run_config["solids"]["pre_process_metadata"]["config"]["input_prefix"] = partition.value
+    run_config["solids"]["pre_process_metadata"]["config"]["input_prefix"] = partition_key
     creation_date = datetime.now().strftime("%Y%m%d%H%M")
 
     load_prefix = f"dcp_release_{creation_date}"
@@ -28,14 +28,14 @@ def run_config_for_dcp_release_partition(partition: Partition) -> DagsterObjectC
 
 
 # for production releases - uses prod config
-def run_config_for_dcp_release_per_project_partition(partition: Partition) -> DagsterObjectConfigSchema:
-    path = file_relative_path(
+def run_config_for_dcp_release_per_project_partition(partition_key:str) -> DagsterObjectConfigSchema:
+    path = dg.file_relative_path(
         __file__, os.path.join("./run_config/prod", "per_project_dcp_release.yaml")
     )
 
     # jscpd:ignore-start
     run_config: DagsterObjectConfigSchema = load_yaml_from_path(path)
-    run_config["solids"]["pre_process_metadata"]["config"]["input_prefix"] = partition.value
+    run_config["solids"]["pre_process_metadata"]["config"]["input_prefix"] = partition_key
     creation_date = datetime.now().strftime("%Y%m%d%H%M")
 
     load_prefix = f"dcp_release_{creation_date}"
@@ -43,7 +43,7 @@ def run_config_for_dcp_release_per_project_partition(partition: Partition) -> Da
     run_config["resources"]["load_tag"]["config"]["load_tag_prefix"] = load_prefix
 
     # TODO this is kind of a hack; we're looking for a UUID in the source path and assuming it's a project ID
-    project_id = find_project_id_in_str(partition.value)
+    project_id = find_project_id_in_str(partition_key)
     run_config["resources"]["hca_project_id"]["config"]["hca_project_id"] = project_id
     # jscpd:ignore-end
 
@@ -52,14 +52,14 @@ def run_config_for_dcp_release_per_project_partition(partition: Partition) -> Da
 
 # for dev releases - uses dev config
 # Definitely hacky and very not dry - but this is all going away, and it's how it was done before
-def dev_run_config_for_dcp_release_per_project_partition(partition: Partition) -> DagsterObjectConfigSchema:
-    path = file_relative_path(
+def dev_run_config_for_dcp_release_per_project_partition(partition_key:str) -> DagsterObjectConfigSchema:
+    path = dg.file_relative_path(
         __file__, os.path.join("./run_config/dev", "per_project_dcp_release_dev.yaml")
     )
     # jscpd:ignore-start
     # ignore that this is not DRY
     run_config: DagsterObjectConfigSchema = load_yaml_from_path(path)
-    run_config["solids"]["pre_process_metadata"]["config"]["input_prefix"] = partition.value
+    run_config["solids"]["pre_process_metadata"]["config"]["input_prefix"] = partition_key
     creation_date = datetime.now().strftime("%Y%m%d%H%M")
 
     load_prefix = f"dcp_release_{creation_date}"
@@ -67,7 +67,7 @@ def dev_run_config_for_dcp_release_per_project_partition(partition: Partition) -
     run_config["resources"]["load_tag"]["config"]["load_tag_prefix"] = load_prefix
 
     # TODO this is kind of a hack; we're looking for a UUID in the source path and assuming it's a project ID
-    project_id = find_project_id_in_str(partition.value)
+    project_id = find_project_id_in_str(partition_key)
     run_config["resources"]["hca_project_id"]["config"]["hca_project_id"] = project_id
     # jscpd:ignore-end
 
@@ -75,14 +75,14 @@ def dev_run_config_for_dcp_release_per_project_partition(partition: Partition) -
 
 
 # run config for make_snapshot_public_job
-def run_config_per_project_public_snapshot_job(partition: Partition) -> DagsterObjectConfigSchema:
-    path = file_relative_path(
+def run_config_per_project_public_snapshot_job(partition_key) -> DagsterObjectConfigSchema:
+    path = dg.file_relative_path(
         __file__, os.path.join("./run_config/prod", "per_project_public_snapshot.yaml")
     )
     # jscpd:ignore-start
     run_config: DagsterObjectConfigSchema = load_yaml_from_path(path)
     # we bake the release tag into the uploaded partitions csv (i.e, <uuid>,<release tag>)
-    project_id, release_tag = partition.value.split(',')
+    project_id, release_tag = partition_key.split(',')
     run_config["resources"]["snapshot_config"]["config"]["source_hca_project_id"] = project_id
     run_config["resources"]["snapshot_config"]["config"]["qualifier"] = release_tag
     # jscpd:ignore-end
